2c8eefc1e2649aebdd9083cbd4b2f2523c6bb296
Philip Patsch <philip.patsch@tweag.io>
2019-01-23 13:17:30 +0100

    nix/nixpkgs.nix: use fetchTarball instead of fetchGit
    fetchGit clones the whole of nixpkgs, which take a *lot* more time
    than fetching the revision tarball that github can generate.

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
-  src = builtins.fetchGit {
-    url = "http://github.com/NixOS/nixpkgs";
-    rev = "a06b90a7dc683f303f2135c8dc849616a11b161a";
+  src = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/a06b90a7dc683f303f2135c8dc849616a11b161a.tar.gz";
+    sha256 = "0pnnrqf487r5vffi9gk2m30z9sqxam8nh4aaafx8s9v4qz13v2y3";
   };
 in
   import src
