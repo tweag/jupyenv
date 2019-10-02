1aeb2a2e80daf686c008e999a58c46fbb0240b2c
guaraqe <mail@guaraqe.gq>
2019-10-02 15:29:46 +0200

    Make most overrides local
    This moves most of the overrides from the haskell overlay level to the
    per project shell.nix level.
    
    This will simplify the updating of Nixpkgs later.

--- example/Haskell/dataHaskell/shell.nix
+++ example/Haskell/dataHaskell/shell.nix
@@ -2,7 +2,20 @@ let
   jupyterLibPath = ../../..;
   nixpkgsPath = jupyterLibPath + "/nix";
   pkgs = import nixpkgsPath {};
-  jupyter = import jupyterLibPath { pkgs=pkgs; };
+  jupyter = import jupyterLibPath {
+    overlays = [ (import ./overlay.nix) ];
+  };
+
+  dataHaskellCoreSrc = pkgs.fetchFromGitHub {
+    owner = "DataHaskell";
+    repo = "dh-core";
+    rev = "3fd4d8d62e12452745dc484459d1a5874f523df9";
+    sha256 = "12z0jfhwpvk5gd1wckasy346aqm0280pv5h7jl1grpk797zjdswx";
+  };
+
+  hspkgs = pkgs.haskellPackages;
+  dh-core = hspkgs.callCabal2nix "dh-core" "${dataHaskellCoreSrc}/dh-core" {};
+  analyze = hspkgs.callCabal2nix "analyze" "${dataHaskellCoreSrc}/analyze" {};
 
   ihaskellWithPackages = jupyter.kernels.iHaskellWith {
       name = "dataHaskell";
