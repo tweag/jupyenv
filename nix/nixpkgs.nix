d5925e1852e96a8b2eb4585c38033c59601331ce
MMesch <MMesch@users.noreply.github.com>
2019-11-15 00:04:45 +0100

    bump nixpkgs and jupyterlab to 1.2.2

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
   src = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a1c6434ebd46c455ff41bd6daa97f0a55df0d8c5.tar.gz";
-    sha256 = "0kk6j5k1czz6a758vbzbq6dpfssm92rmagr62ljapav1yx0bah3b";
+    url = "https://github.com/NixOS/nixpkgs/archive/db3beda6b56563777254156351b9bed349cf7d07.tar.gz";
+    sha256 = "0lkxj9pkzwxbihn3j5nyk61814m5kshpziagdzicxlns1gn1295i";
   };
 in
   import src
