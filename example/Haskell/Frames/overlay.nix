917d403e8a8603aced9fc8c45ab62060a5c780ec
guaraqe <mail@guaraqe.gq>
2019-11-20 18:04:40 +0100

    fix frames example

--- example/Haskell/Frames/overlay.nix
+++ example/Haskell/Frames/overlay.nix
@@ -2,9 +2,11 @@ _: pkgs:
 let
   dontCheck = pkgs.haskell.lib.dontCheck;
   overrides = _: hspkgs: {
-      vinyl_0_10_0 = hspkgs.vinyl_0_10_0_1;
       singletons = dontCheck (hspkgs.callHackage "singletons" "2.4.1" {});
       th-desugar = hspkgs.callHackage "th-desugar" "1.8" {};
+      Frames = hspkgs.callHackage "Frames" "0.5.1" {};
+      htoml = hspkgs.callHackage "htoml" "1.0.0.2" {};
+      vinyl = dontCheck (hspkgs.callHackage "vinyl" "0.10.0.1" {});
   };
 in
 {
