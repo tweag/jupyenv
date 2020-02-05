433a3eded64687bdf0f215c8e3a3bfed64b9d807
Junji Hashimoto <junji.hashimoto@gree.net>
2020-02-05 19:47:40 +0900

    Add nix-shell for CI

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  jupyter = import ./. {};
+
+  iPython = jupyter.kernels.iPythonWith {
+    name = "python";
+    packages = p: with p; [ numpy ];
+  };
+
+  iHaskell = jupyter.kernels.iHaskellWith {
+    name = "haskell";
+    packages = p: with p; [ hvega formatting ];
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iPython iHaskell ];
+    };
+in
+  jupyterEnvironment.env
