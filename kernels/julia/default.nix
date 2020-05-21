e539d43d1fa5f2b9fd0736943942bb2477c79644
GTrunsec <gtrunsec@hardenedlinux.org>
2020-05-20 18:17:13 -0700

    Julia: Optional Cuda nixpkgs

--- kernels/julia/default.nix
+++ kernels/julia/default.nix
@@ -1,5 +1,5 @@
-{ nixpkgs ? import (builtins.fetchTarball "https://github.com/GTrunSec/nixpkgs/tarball/39247f8d04c04b3ee629a1f85aeedd582bf41cac"){}
-, cudapkgs ? import (builtins.fetchTarball "https://github.com/GTrunSec/nixpkgs/tarball/927fcf37933ddd24a0e16c6a45b9c0a687a40607"){}
+{ nixpkgs 
+, cudapkgs
 , stdenv
 , name ? "nixpkgs"
 , packages ? (_:[])
@@ -9,6 +9,7 @@
 , directory
 , NUM_THREADS ? 1
 , cuda ? false
+, cudaVersion 
 }:
 
 let
@@ -24,8 +25,6 @@ let
   ] ++ (extraPackages  nixpkgs) ++ nixpkgs.lib.optionals cuda
     [
       # Flux.jl
-      cudatoolkit_10_2
-      cudapkgs.linuxPackages.nvidia_x11_beta
       libGLU
 		  xorg.libXi xorg.libXmu freeglut
       xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib
@@ -39,21 +38,18 @@ let
  julia_wrapped = nixpkgs.stdenv.mkDerivation rec {
     name = "julia_wrapped";
     buildInputs = [julia ] ++ extraLibs;
-    nativeBuildInputs = with nixpkgs; [ makeWrapper cacert git pkgconfig which cudapkgs.linuxPackages.nvidia_x11_beta];
+    nativeBuildInputs = with nixpkgs; [ makeWrapper cacert git pkgconfig which ];
     phases = [ "installPhase" ];
     installPhase = ''
-      export CUDA_PATH="${cudapkgs.cudatoolkit_10_2}"
-      export EXTRA_LDFLAGS="-L/lib -L${cudapkgs.linuxPackages.nvidia_x11_beta}/lib"
-      export EXTRA_CCFLAGS="-I/usr/include"
       export LD_LIBRARY_PATH=${nixpkgs.lib.makeLibraryPath extraLibs}
        ${if cuda then ''
       makeWrapper ${julia}/bin/julia $out/bin/julia_wrapped \
       --set JULIA_DEPOT_PATH ${directory} \
       --prefix LD_LIBRARY_PATH : "$LD_LIBRARY_PATH" \
-      --prefix LD_LIBRARY_PATH ":" "${cudapkgs.linuxPackages.nvidia_x11_beta}/lib" \
+      --prefix LD_LIBRARY_PATH ":" "${cudapkgs.linuxPackages.nvidia_x11}/lib" \
       --set JULIA_PKGDIR ${directory} \
       --set JULIA_NUM_THREADS ${toString NUM_THREADS} \
-       --set CUDA_PATH "${cudapkgs.cudatoolkit_10_2}"
+       --set CUDA_PATH "${cudaVersion}"
       ''
          else ''
       makeWrapper ${julia}/bin/julia $out/bin/julia_wrapped \
@@ -98,8 +94,9 @@ let
     && ${julia_wrapped}/bin/julia_wrapped -e 'using Pkg; Pkg.test("CUDAnative");'
   get_nvdisasm=$(dirname ${directory}/artifacts/*/bin/nvdisasm)
   rm -rf $get_nvdisasm/nvdisasm
-  ln -s ${cudapkgs.cudatoolkit_10_2}/bin/nvdisasm  $get_nvdisasm/nvdisasm
+  ln -s ${cudaVersion}/bin/nvdisasm  $get_nvdisasm/nvdisasm
   '';
+
   InstalliJulia = writeScriptBin "Install_iJulia" ''
      if [ ! -d "${directory}/registries/Genera/" ]; then
      mkdir -p ${directory}/registries/General && git clone https://github.com/JuliaRegistries/General.git --depth=1 ${directory}/registries/General
