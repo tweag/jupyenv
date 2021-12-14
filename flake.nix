9b5fad07df638bbd03ecdad2f4648a757abfaa73
GTrunSec <gtrunsec@hardenedlinux.org>
2021-12-14 11:22:36 -0800

    resolve change request

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "declarative and reproducible Jupyter environments - powered by Nix";
 
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
-      url = github:/teto/flake-compat/support-packages;
+      url = "github:/teto/flake-compat/support-packages";
       flake = false;
     };
     nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
-    ihaskell.url = github:gibiansky/IHaskell;
+    ihaskell.url = "github:gibiansky/IHaskell";
   };
 
   outputs =
@@ -18,7 +18,7 @@
     , flake-utils
     , ...
     }:
-    (flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"]
+    (flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
       (system:
       let
         pkgs = import nixpkgs
@@ -26,38 +26,36 @@
             inherit system;
             allowUnsupportedSystem = true;
             overlays = nixpkgs.lib.attrValues self.overlays;
-              # [ self.overlays.jupyterWith ];
+            # [ self.overlays.jupyterWith ];
           };
         ihaskellOverlay = ihaskell.packages.${system}.ihaskell-env.ihaskellOverlay;
 
       in
       {
 
-        packages = let
-          iPython = pkgs.jupyterWith.kernels.iPythonWith {
-            name = "Python-data-env";
-            ignoreCollisions = true;
-          };
-
-          iHaskell = pkgs.jupyterWith.kernels.iHaskellWith {
-            name = "ihaskell-flake";
-            packages = p: with p; [ vector aeson ];
-            extraIHaskellFlags = "--codemirror Haskell"; # for jupyterlab syntax highlighting
-            haskellPackages = pkgs.haskellPackages;
-          };
-
-        in
-        {
-          jupyterEnvironment = pkgs.jupyterWith.jupyterlabWith {
-              kernels = [ iPython iHaskell ];
-              directory = "./.jupyterlab";
+        packages =
+          let
+            iPython = pkgs.jupyterWith.kernels.iPythonWith {
+              name = "Python-data-env";
+              ignoreCollisions = true;
             };
-        };
+
+            iHaskell = pkgs.jupyterWith.kernels.iHaskellWith {
+              name = "ihaskell-flake";
+              packages = p: with p; [ vector aeson ];
+              extraIHaskellFlags = "--codemirror Haskell"; # for jupyterlab syntax highlighting
+              haskellPackages = pkgs.haskellPackages;
+            };
+
+          in
+          {
+            jupyterEnvironment = pkgs.jupyterWith.jupyterlabWith {
+              kernels = [ iPython iHaskell ];
+            };
+          };
 
         defaultPackage = self.packages."${system}".jupyterEnvironment;
-
-      }
-      )
+      })
     ) //
     {
       overlays = {
@@ -65,13 +63,13 @@
           jupyterWith = prev.callPackage ./. { pkgs = final; };
 
           inherit (jupyterWith)
-              jupyterlabWith
-              kernels
-              mkBuildExtension
-              mkDirectoryWith
-              mkDirectoryFromLockFile
-              mkDockerImage
-              ;
+            jupyterlabWith
+            kernels
+            mkBuildExtension
+            mkDirectoryWith
+            mkDirectoryFromLockFile
+            mkDockerImage
+            ;
         };
 
         # haskell = import ./nix/haskell-overlay.nix;
@@ -79,9 +77,9 @@
           haskellPackages = prev.haskellPackages.override (old: {
             overrides =
               prev.lib.composeExtensions
-                (old.overrides or (_: _: {}))
+                (old.overrides or (_: _: { }))
                 ihaskell.packages."${prev.system}".ihaskell-env.ihaskellOverlay;
-              });
+          });
         };
 
         python = import ./nix/python-overlay.nix;
