637282fac3216fa76f4d834705f49bbceb130a67
Etienne JEAN <et.jean@outlook.fr>
2022-01-21 12:47:25 +0100

    Fix ruby kernel

--- kernels/iruby/gemdir/generate.sh
+++ kernels/iruby/gemdir/generate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#! nix-shell --pure -I nixpkgs=../../nix -i bash -p bundler -p bundix
+#! nix-shell --pure -I nixpkgs=../../../nix/nixpkgs.nix -i bash -p bundler -p bundix
 
 bundle lock
 bundix
