d87ac17c8426e2351d67c7f9650a7a0241dc51ea
Rok Garbas <rok@garbas.si>
2022-05-25 17:46:45 +0200

    Switch to different cachix secret (#18)
    * secrets.CACHIX_AUTH_TOKEN -> secrets.CACHIX_TWEAG_JUPYTER_NIX_AUTH_TOKEN
    
    also binary cache was renamed (tweag-jupyter-nix -> tweag-jupyter)
    
    * tweag-jupyter -> tweag-jupyter-nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "declarative and reproducible Jupyter environments - powered by Nix";
 
-  nixConfig.extra-substituters = "https://tweag-jupyter-nix.cachix.org";
-  nixConfig.extra-trusted-public-keys = "tweag-jupyter-nix.cachix.org-1:rlkzt5dp3c4yiceqd6btz3mtzkoqiw6nmeab/o6tfxg=";
+  nixConfig.extra-substituters = "https://tweag-jupyter.cachix.org";
+  nixConfig.extra-trusted-public-keys = "tweag-jupyter.cachix.org-1:UtNH4Zs6hVUFpFBTLaA4ejYavPo5EFFqgd7G7FxGW9g=";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-21.11";
