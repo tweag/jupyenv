ac6649bea5259f5ac200acbf9298dfd25d574a1a
Daniel Baker <daniel.baker@tweag.io>
2022-06-23 21:52:47 -0700

    Fixed system monitor derivation so yarn2nix will take it. Still crashes near the end of build.

--- extensions/jupyterlab-system-monitor/default.nix
+++ extensions/jupyterlab-system-monitor/default.nix
@@ -2,17 +2,22 @@
   name = "jupyterlab-system-monitor";
   version = "0.8.0";
 
-  extension = pkgs.fetchFromGitHub {
-    owner = "jtpio";
-    repo = "${name}";
-    rev = "refs/tags/${version}";
-    sha256 = "0jv393bbjx8jda1nwsym3n37f5h47p63zdvw07vnxranwrpq41xl";
-    #fetchSubmodules = true;
-    postFetch = ''
-      # give the package a name for yarn
+  extension = pkgs.stdenv.mkDerivation {
+    name = "${name}-drv";
+    src = pkgs.fetchFromGitHub {
+      owner = "jtpio";
+      repo = "${name}";
+      rev = "refs/tags/${version}";
+      sha256 = "0jv393bbjx8jda1nwsym3n37f5h47p63zdvw07vnxranwrpq41xl";
+    };
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out
+    '';
+    postPatch = ''
+      # give the package.json a name and version for yarn2nix
+      sed -i '0,/{/a\  "version": "${version}",' package.json
       sed -i '0,/{/a\  "name": "${name}",' package.json
-      head package.json
-      exit 123
     '';
   };
 in
