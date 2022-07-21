578e24464d6bc3c7f61c5d271bd3a2117fca93f1
Daniel Baker <daniel.baker@tweag.io>
2022-07-21 16:04:45 -0700

    still not working but making progress.

--- extensions/jupyterlab-theme-solarized-dark/default.nix
+++ extensions/jupyterlab-theme-solarized-dark/default.nix
@@ -2,20 +2,6 @@
   name-ext = "jupyterlab-theme-solarized-dark";
   version-ext = "43de0c993ed3266e36b8dc5b5a2a7479ef67deda";
 
-  extension = pkgs.stdenv.mkDerivation {
-    name = "${name-ext}-drv";
-    src = pkgs.fetchFromGitHub {
-      owner = "AllanChain";
-      repo = "${name-ext}";
-      rev = "${version-ext}";
-      sha256 = "sha256-N5R1AbDctso3uiV3kTnvLCpCl/5kAaEgEOht7+qT9v8=";
-    };
-    installPhase = ''
-      mkdir -p $out
-      cp -r * $out
-    '';
-  };
-
   name-jlab = "jupyterlab";
   version-jlab = "3.4.3";
 
@@ -33,26 +19,53 @@
     '';
   };
 
-  jupyterlabStaging = pkgs.mkYarnPackage {
-    name = "${name-jlab}-staging";
-    src = "${jupyterlabStagingFiles}";
-    postBuild = ''
-      ls -Fho deps/@${name-jlab}/application-top/deps/@{name-jlab}
-      exit 123
-    '';
+  #  jupyterlabStaging = pkgs.mkYarnPackage {
+  #    name = "${name-jlab}-staging";
+  #    src = "${jupyterlabStagingFiles}";
+  #    postBuild = ''
+  #      ls -Fho deps/@${name-jlab}/application-top/deps/@{name-jlab}
+  #      exit 123
+  #    '';
+  #  };
+
+  #  pkgs.yarn2nix-moretea.mkYarnModules {
+  #    pname = "jlab-modules";
+  #    version = "1.0.0";
+  #    packageJSON = "${jupyterlabStagingFiles}/package.json";
+  #    yarnLock = "${jupyterlabStagingFiles}/yarn.lock";
+  #  }
+
+  # checking the result of yarn.nix to test an offline mirror
+  # pkgs.yarn2nix-moretea.mkYarnNix {yarnLock = "${jupyterlabStagingFiles}/yarn.lock";}
+
+  extension = pkgs.fetchFromGitHub {
+    owner = "AllanChain";
+    repo = "${name-ext}";
+    rev = "${version-ext}";
+    sha256 = "sha256-N5R1AbDctso3uiV3kTnvLCpCl/5kAaEgEOht7+qT9v8=";
+  };
+
+  nodeModules = pkgs.mkYarnModules {
+    pname = name-ext;
+    version = version-ext;
+    packageJSON = "${extension}/package.json";
+    yarnLock = "${extension}/yarn.lock";
   };
 in
-  # checking the result of yarn.nix to test an offline mirror
-  pkgs.yarn2nix-moretea.mkYarnNix {yarnLock = "${jupyterlabStagingFiles}/yarn.lock";}
-#  pkgs.mkYarnPackage rec {
-#    name = name-ext;
-#    src = "${extension}";
-#
-#    buildPhase = ''
-#      pushd deps/${name}
-#      node ${jupyterlabStagingFiles}/yarn.js --offline tsc
-#      # node ${jupyterlabStagingFiles}/yarn.js --offline
-#      popd
-#    '';
-#  }
+  pkgs.runCommand "${name-ext}-${version-ext}" {buildInputs = [pkgs.python3Packages.jupyterlab pkgs.python3Packages.jupyter pkgs.nodejs];} ''
+    mkdir extension .yarn
+    export HOME=$PWD
+    cp -R ${extension}/* extension
 
+    pushd extension
+
+    ln -s ${nodeModules}/node_modules ./node_modules
+
+    ./node_modules/.bin/tsc
+
+    jupyter labextension build .
+
+    popd
+
+    mkdir $out
+  ''
