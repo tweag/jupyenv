d9f207892543c152cca8b396cc612bb4f23e7656
Daniel Baker <daniel.baker@tweag.io>
2022-08-02 13:30:16 -0700

    Modified the rust kernel to use runtime packages.

--- kernels/rust/default.nix
+++ kernels/rust/default.nix
@@ -7,20 +7,44 @@
   name ? "rust",
   displayName ? "Rust", # TODO: add Rust version
   language ? "rust",
-  argv ? [
-    "${evcxr}/bin/evcxr_jupyter"
-    "--control_file"
-    "{connection_file}"
-  ],
+  argv ? null,
   codemirrorMode ? "rust",
   logo64 ? ./logo64.png,
   logo32 ? ./logo32.png,
-}: {
+  runtimePackages ? with pkgs; [cargo gcc binutils-unwrapped],
+  extraRuntimePackages ? [],
+}: let
+  allRuntimePackages = runtimePackages ++ extraRuntimePackages;
+
+  env = evcxr;
+  wrappedEnv =
+    pkgs.runCommand "wrapper-${env.name}"
+    {nativeBuildInputs = [pkgs.makeWrapper];}
+    ''
+      mkdir -p $out/bin
+      for i in ${env}/bin/*; do
+        filename=$(basename $i)
+        ln -s ${env}/bin/$filename $out/bin/$filename
+        wrapProgram $out/bin/$filename \
+          --set PATH "${pkgs.lib.makeSearchPath "bin" allRuntimePackages}" \
+          --set RUST_SRC_PATH "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}"
+      done
+    '';
+
+  argv_ =
+    if argv == null
+    then [
+      "${wrappedEnv}/bin/evcxr_jupyter"
+      "--control_file"
+      "{connection_file}"
+    ]
+    else argv;
+in {
+  argv = argv_;
   inherit
     name
     displayName
     language
-    argv
     codemirrorMode
     logo64
     ;
