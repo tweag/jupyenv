313c1aad5ea64b17dc6f7d18dd54b559fe5c1a25
Dan Baker <daniel.n.baker@gmail.com>
2022-08-08 13:30:42 -0700

    next-feature/add-runtime-packages (#244)
    * Updated nix and bash kernels to have extra runtime packages argument.
    
    * Adding ansible as a runtime package fixes the ansible kernel.
    
    * Modified the rust kernel to use runtime packages.
    
    * Added a rust overlay to nixpkgs to get the rust kernel to work properly.
    
    * Made the rust overlay pure.
    
    * Changing rust package used based on rust-overlay recommendation.
    
    * Refactored nix and bash kernels so runtime packages are consistent.
    
    Co-authored-by: Daniel Baker <daniel.baker@tweag.io>
    Co-authored-by: Rok Garbas <rok@garbas.si>

--- kernels/nix/default.nix
+++ kernels/nix/default.nix
@@ -24,38 +24,49 @@
       preferWheels
       ;
   };
-
-  nix-bin =
-    pkgs.runCommand "wrapper-${env.name}"
-    {nativeBuildInputs = [pkgs.makeWrapper];}
-    ''
-      mkdir -p $out/bin
-      for i in ${env}/bin/*; do
-        filename=$(basename $i)
-        ln -s ${env}/bin/$filename $out/bin/$filename
-        wrapProgram $out/bin/$filename \
-          --set PATH ${nix}/bin
-      done
-    '';
 in
   {
     name ? "nix",
     displayName ? "Nix", # TODO: add Nix version
     language ? "Nix",
-    argv ? [
-      "${nix-bin}/bin/python"
-      "-m"
-      "nix-kernel"
-      "-f"
-      "{connection_file}"
-    ],
+    argv ? null,
     logo64 ? ./logo64.png,
-  }: {
+    runtimePackages ? [nix],
+    extraRuntimePackages ? [],
+    nixpkgsPath ? pkgs.path,
+  }: let
+    allRuntimePackages = runtimePackages ++ extraRuntimePackages;
+
+    wrappedEnv =
+      pkgs.runCommand "wrapper-${env.name}"
+      {nativeBuildInputs = [pkgs.makeWrapper];}
+      ''
+        mkdir -p $out/bin
+        for i in ${env}/bin/*; do
+          filename=$(basename $i)
+          ln -s ${env}/bin/$filename $out/bin/$filename
+          wrapProgram $out/bin/$filename \
+            --set PATH "${pkgs.lib.makeSearchPath "bin" allRuntimePackages}"\
+            --set NIX_PATH "nixpkgs=${nixpkgsPath}"
+        done
+      '';
+
+    argv_ =
+      if argv == null
+      then [
+        "${wrappedEnv}/bin/python"
+        "-m"
+        "nix-kernel"
+        "-f"
+        "{connection_file}"
+      ]
+      else argv;
+  in {
+    argv = argv_;
     inherit
       name
       displayName
       language
-      argv
       logo64
       ;
   }
--- kernels/rust/default.nix
+++ kernels/rust/default.nix
@@ -7,20 +7,59 @@
   name ? "rust",
   displayName ? "Rust", # TODO: add Rust version
   language ? "rust",
-  argv ? [
-    "${evcxr}/bin/evcxr_jupyter"
-    "--control_file"
-    "{connection_file}"
-  ],
+  argv ? null,
   codemirrorMode ? "rust",
   logo64 ? ./logo64.png,
   logo32 ? ./logo32.png,
-}: {
+  runtimePackages ? with pkgs; [cargo gcc binutils-unwrapped],
+  extraRuntimePackages ? [],
+}: let
+  rust-overlay = import (builtins.fetchTarball {
+    url = "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+    sha256 = "10ja06zgc7y53715vwhxz29knbijyikjzldjng0k9zm2hcksaxfr";
+  });
+  pkgs-rust = pkgs.extend rust-overlay;
+
+  /*
+   rust-overlay recommends using `default` over `rust`.
+   Pre-aggregated package `rust` is not encouraged for stable channel since it
+   contains almost all and uncertain components.
+   https://github.com/oxalica/rust-overlay/blob/1558464ab660ddcb45a4a4a691f0004fdb06a5ee/rust-overlay.nix#L331
+   */
+  rust = pkgs-rust.rust-bin.stable.latest.default.override {
+    extensions = ["rust-src"];
+  };
+  allRuntimePackages = runtimePackages ++ extraRuntimePackages ++ [rust];
+  env = evcxr;
+in let
+  wrappedEnv =
+    pkgs.runCommand "wrapper-${env.name}"
+    {nativeBuildInputs = [pkgs.makeWrapper];}
+    ''
+      mkdir -p $out/bin
+      for i in ${env}/bin/*; do
+        filename=$(basename $i)
+        ln -s ${env}/bin/$filename $out/bin/$filename
+        wrapProgram $out/bin/$filename \
+          --set PATH "${pkgs.lib.makeSearchPath "bin" allRuntimePackages}" \
+          --set RUST_SRC_PATH "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}"
+      done
+    '';
+
+  argv_ =
+    if argv == null
+    then [
+      "${wrappedEnv}/bin/evcxr_jupyter"
+      "--control_file"
+      "{connection_file}"
+    ]
+    else argv;
+in {
+  argv = argv_;
   inherit
     name
     displayName
     language
-    argv
     codemirrorMode
     logo64
     ;
