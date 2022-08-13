0beffa46f47df313d70577c44d915d98178967c3
Daniel Baker <daniel.baker@tweag.io>
2022-08-12 22:16:55 -0700

    Used node2nix to generate derivations.

---
+++ kernels/typescript/generate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+rm -f ./node-env.nix
+
+# Track the latest active nodejs LTS here: https://nodejs.org/en/about/releases/
+nix run nixpkgs#node2nix -- \
+    -i node-packages.json \
+    -o node-packages.nix \
+    -c composition.nix \
+    --pkg-name nodejs-14_x
+
---
+++ kernels/typescript/overrides.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  nodejs,
+}: let
+  inherit
+    (pkgs)
+    stdenv
+    lib
+    callPackage
+    fetchFromGitHub
+    fetchurl
+    nixosTests
+    ;
+
+  since = version: lib.versionAtLeast nodejs.version version;
+  before = version: lib.versionOlder nodejs.version version;
+in
+  final: prev: {
+    tslab = prev.tslab.override (oldAttrs: {
+      preRebuild = ''
+        export NPM_CONFIG_ZMQ_EXTERNAL=true
+      '';
+      buildInputs = oldAttrs.buildInputs ++ [final.node-gyp-build pkgs.zeromq];
+    });
+  }
