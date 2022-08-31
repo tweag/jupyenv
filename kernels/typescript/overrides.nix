f385eee9361fdcd8ca02e32afc08e1d17aa99c4b
Daniel Baker <daniel.baker@tweag.io>
2022-08-31 13:06:45 -0700

    cleaned out old code.

--- kernels/typescript/overrides.nix
+++ kernels/typescript/overrides.nix
@@ -1,24 +1,11 @@
 {
   pkgs,
   nodejs ? pkgs."nodejs-14_x",
-}: let
-  # inherit
-  #   (pkgs)
-  #   stdenv
-  #   lib
-  #   callPackage
-  #   fetchFromGitHub
-  #   fetchurl
-  #   nixosTests
-  #   ;
-  # since = version: lib.versionAtLeast nodejs.version version;
-  # before = version: lib.versionOlder nodejs.version version;
-in
-  final: prev: {
-    tslab = prev.tslab.override (oldAttrs: {
-      preRebuild = ''
-        export NPM_CONFIG_ZMQ_EXTERNAL=true
-      '';
-      buildInputs = oldAttrs.buildInputs ++ [final.node-gyp-build pkgs.zeromq];
-    });
-  }
+}: final: prev: {
+  tslab = prev.tslab.override (oldAttrs: {
+    preRebuild = ''
+      export NPM_CONFIG_ZMQ_EXTERNAL=true
+    '';
+    buildInputs = oldAttrs.buildInputs ++ [final.node-gyp-build pkgs.zeromq];
+  });
+}
