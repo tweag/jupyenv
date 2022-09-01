773fe64eb35cab7d13ebc548de8f53c2bb14627d
Daniel Baker <daniel.baker@tweag.io>
2022-09-01 15:10:14 -0700

    Got the typescript kernel working with help from @infinisil.

--- kernels/typescript/default.nix
+++ kernels/typescript/default.nix
@@ -2,13 +2,40 @@
   self,
   pkgs,
   nodejs ? pkgs.nodejs-14_x,
+  npmlock2nix ? pkgs.npmlock2nix,
+  yarn ? pkgs.yarn,
 }: let
-  inherit (pkgs) fetchurl lib stdenv writeScriptBin;
+  inherit (pkgs) lib stdenv writeScriptBin;
   inherit (lib) makeBinPath;
 
+  tslabSrc = fetchTarball {
+    url = "https://github.com/yunabe/tslab/archive/f056a88baf81a1fc6a01b0446a3febcf919ff8e4.tar.gz";
+    sha256 = "1q2wsdcgha6qivs238pysgmiabjhyflpd1bqbx0cgisgiz2nq3vs";
+  };
+
   tslab = pkgs.npmlock2nix.build {
-    src = ./.;
-    installPhase = "cp -r dist $out";
+    src = tslabSrc;
+    #patches = [ ./kernels/typescript/tslab-version.patch ];
+    #postPatch = ''
+    #  substitute ${./kernels/typescript/tslab-version.patch} tslab-version.patch \
+    #    --subst-var-by version "1.0.15"
+    #  patch -p1 tslab-version.patch
+    #'';
+    node_modules_attrs.packageLockJson = ./package-lock.json;
+    buildInputs = [pkgs.makeWrapper];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r dist $out/libexec
+      substitute bin/tslab $out/bin/tslab \
+        --replace "../dist/main.js" "../libexec/main.js"
+      chmod +x $out/bin/tslab
+      wrapProgram $out/bin/tslab \
+        --set NODE_PATH $out/libexec/node_modules \
+        --chdir $out/libexec
+      ln -s ${tslab.node_modules}/node_modules $out/libexec/node_modules
+      cp package.json $out # FIXME
+    '';
+    # buildCommands = [ "yarn build" ];
   };
 
   tslabSh = writeScriptBin "tslab" ''
@@ -29,7 +56,7 @@ in
   }: let
     allRuntimePackages = runtimePackages ++ extraRuntimePackages;
 
-    env = tslabSh;
+    env = tslab;
     wrappedEnv =
       pkgs.runCommand "wrapper-${env.name}"
       {nativeBuildInputs = [pkgs.makeWrapper];}
