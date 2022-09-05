882949734557dbf142bcefc286ecd35b8e22989d
José Luis Lafuente <jose.lafuente@tweag.io>
2022-09-05 16:18:49 +0200

    Fix Elm test

--- kernels/elm/test.py
+++ kernels/elm/test.py
@@ -3,12 +3,11 @@ from testbook import testbook
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-@testbook(f'{current_dir}/test.ipynb', execute=True, kernel_name="example_elm")
+@testbook(f"{current_dir}/test.ipynb", execute=True, kernel_name="example_elm", timeout=240)
 def test_nb(tb):
-    result = tb.cell_output_text(0)
-    print("Output:")
-    print(result)
-    assert result == "hello elm"
+    result = tb.execute_cell(0)["outputs"][0]["data"]
+    assert "text/html" in result
+    assert "elm-div" in result["text/html"]
 
 if __name__ == '__main__':
     test_nb()
