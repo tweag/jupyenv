0ca9d99dc0494656b04d110b1ec4fa1743eb52d8
Dan Baker <daniel.n.baker@gmail.com>
2022-10-07 00:30:38 +0200

    Update bash kernel to 0.8.0. (#310)
    Co-authored-by: Rok Garbas <rok@garbas.si>

--- kernels/available/bash/overrides.nix
+++ kernels/available/bash/overrides.nix
@@ -5,17 +5,8 @@ final: prev: let
     });
   };
 in
-  {
-    bash-kernel = prev.bash-kernel.overridePythonAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.flit-core final.ipykernel];
-      postPatch = ''
-        sed -i \
-          -e 's|requires = \["flit"\]|requires = \["flit_core >=3.2,<4"\]|' \
-          -e 's|build-backend = "flit.buildapi"|build-backend = "flit_core.buildapi"|' \
-            pyproject.toml
-      '';
-    });
-  }
+  {}
+  // addNativeBuildInputs "bash-kernel" [final.flit-core final.ipykernel]
   // addNativeBuildInputs "traitlets" [final.hatchling]
   // addNativeBuildInputs "jupyter-client" [final.hatchling]
   // addNativeBuildInputs "ipykernel" [final.hatchling]
