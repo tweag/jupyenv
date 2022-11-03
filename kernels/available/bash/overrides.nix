d40b06d1132b0faf67237885724ee2b36d0ea195
Rok Garbas <rok@garbas.si>
2022-11-03 16:02:16 +0000

    fix building of bash kernel

--- kernels/available/bash/overrides.nix
+++ kernels/available/bash/overrides.nix
@@ -1,12 +1,1 @@
-final: prev: let
-  addNativeBuildInputs = drvName: inputs: {
-    "${drvName}" = prev.${drvName}.overridePythonAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ inputs;
-    });
-  };
-in
-  {}
-  // addNativeBuildInputs "bash-kernel" [final.flit-core final.ipykernel]
-  // addNativeBuildInputs "traitlets" [final.hatchling]
-  // addNativeBuildInputs "jupyter-client" [final.hatchling]
-  // addNativeBuildInputs "ipykernel" [final.hatchling]
+final: prev: {}
