d21bc489f2b233d30e20eff7a138f2e223fc4e92
Rok Garbas <rok@garbas.si>
2022-11-07 20:31:21 +0000

    fix testbook again

--- overrides.nix
+++ overrides.nix
@@ -19,6 +19,18 @@ pkgs: let
     // {
       babel = prev.babel_;
       Babel = prev.babel_;
+      testbook = prev.testbook.overridePythonAttrs (old: {
+        postPatch = ''
+          mkdir ./tmp
+          ${pkgs.unzip}/bin/unzip dist/testbook-${old.version}-py3-none-any.whl -d ./tmp
+          sed -i -e "s|if not any(arg.startswith('--Kernel|if False and not any(arg.startswith('--Kernel|" tmp/testbook/client.py
+          rm dist/testbook-${old.version}-py3-none-any.whl
+          pushd tmp
+            ${pkgs.zip}/bin/zip -r ../dist/testbook-${old.version}-py3-none-any.whl ./*
+          popd
+          rm -rf tmp
+        '';
+      });
     };
 in
   (pkgs.poetry2nix.defaultPoetryOverrides.overrideOverlay preOverlay).extend postOverlay
