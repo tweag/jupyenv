e2063752fa81a4b54fe561691c1024a372d764b4
Rok Garbas <rok@garbas.si>
2022-11-15 11:05:54 +0000

    Another idea how this could work

--- kernels/available/python/overrides.nix
+++ kernels/available/python/overrides.nix
@@ -1,28 +1,16 @@
-pkgs: let
-  addNativeBuildInputs = prev: drvName: inputs: {
-    "${drvName}" = prev.${drvName}.overridePythonAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ inputs;
-    });
-  };
-
-  # A fix is on the way soon, https://github.com/nix-community/poetry2nix/pull/787
-  preOverlay = final: prev: {
-    babel = null;
-    Babel = null;
-    babel_ = prev.babel.overridePythonAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [final.setuptools];
-    });
-    pillow_ = prev.pillow;
-    matplotlib_ = prev.matplotlib;
-  };
-
-  postOverlay = final: prev:
-    {}
-    // {
-      babel = prev.babel_;
-      Babel = prev.babel_;
-      pillow = prev.pillow_;
-      matplotlib = prev.matplotlib_;
-    };
-in
-  (pkgs.poetry2nix.defaultPoetryOverrides.overrideOverlay preOverlay).extend postOverlay
+pkgs: final: prev:
+(pkgs.poetryBuildSystems final prev)
+// {
+  cffi = prev.cffi.overridePythonAttrs (old: {
+    nativeBuildInputs = old.nativeBuildInputs or [] ++ [pkgs.pkg-config final.setuptools];
+    buildInputs = old.buildInputs or [] ++ [pkgs.libffi];
+    prePatch =
+      (old.prePatch or "")
+      + pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+        # Remove setup.py impurities
+        substituteInPlace setup.py --replace "'-iwithsysroot/usr/include/ffi'" ""
+        substituteInPlace setup.py --replace "'/usr/include/ffi'," ""
+        substituteInPlace setup.py --replace '/usr/include/libffi' '${pkgs.lib.getDev pkgs.libffi}/include'
+      '';
+  });
+}
