223c92c175b5c376870e07bb87b3083da9f7d973
Daniel Baker <daniel.baker@tweag.io>
2022-11-22 17:33:47 +0100

    move jupyterlabEnvWrapped to its own nix file.

--- flake.nix
+++ flake.nix
@@ -167,7 +167,6 @@
         pkgs_stable = nixpkgs-stable.legacyPackages.${system};
 
         python = pkgs.python3;
-        python3 = python;
         poetry2nixPkgs = import "${poetry2nix}/default.nix" {inherit pkgs poetry;};
         poetry = pkgs.callPackage "${poetry2nix}/pkgs/poetry" {inherit python;};
 
@@ -191,7 +190,7 @@
           excludes = ["^\\.jupyter/"]; # JUPYTERLAB_DIR
         };
 
-        mkdocs = pkgs.python3.withPackages (p: [
+        mkdocs = python.withPackages (p: [
           p.mkdocs
           p.mkdocs-material
           p.mkdocs-material-extensions
@@ -210,89 +209,7 @@
           '';
         };
 
-        jupyterlabEnvWrapped = {
-          self,
-          system,
-          poetry2nix ? self.inputs.poetry2nix,
-          pkgs ?
-            import self.inputs.nixpkgs {
-              inherit system;
-              overlays = [poetry2nix.overlay];
-            },
-          # https://github.com/nix-community/poetry2nix#mkPoetryEnv
-          projectDir ? self, # TODO: only include relevant files/folders
-          pyproject ? projectDir + "/pyproject.toml",
-          poetrylock ? projectDir + "/poetry.lock",
-          overrides ? import ./overrides.nix pkgs,
-          python ? python3,
-          editablePackageSources ? {},
-          extraPackages ? (ps: []),
-          preferWheels ? false,
-          # groups ? ["devs"], # TODO: add groups after updating to latest poetry2nix. make sure to inherit below
-        }: let
-          jupyterlabEnvBase = pkgs.poetry2nix.mkPoetryEnv {
-            inherit
-              projectDir
-              pyproject
-              poetrylock
-              overrides
-              python
-              editablePackageSources
-              extraPackages
-              preferWheels
-              ;
-          };
-          jupyterlab-checker =
-            pkgs.writeText "jupyterlab-checker"
-            ''
-              from jupyterlab.commands import build_check, ensure_app;
-              import sys
-
-              ensure_app_return = ensure_app('.jupyter/lab/share/jupyter/lab')
-              build_check_return = build_check()
-              if ensure_app_return is None and not build_check_return:
-                sys.exit(0)
-              # print(f'{ensure_app_return = }')
-              # print(f'{build_check_return = }')
-              sys.exit(1)
-            '';
-          jupyterlab-cond-build =
-            pkgs.writeShellScript "jupyterlab-cond-build"
-            ''
-              ${jupyterlabEnvBase}/bin/python ${jupyterlab-checker}
-              checker=$?
-              if [ "$checker" -ne 0 ]
-              then
-                echo jupyterWith needs to build JupyterLab.
-                # we need to build the jupyter lab environment before it can be used
-                ${jupyterlabEnvBase}/bin/jupyter lab build
-              else
-                echo jupyterWith does not need build JupyterLab. Starting...
-              fi
-            '';
-        in
-          pkgs.runCommand "chmod-${jupyterlabEnvBase.name}"
-          {nativeBuildInputs = [pkgs.makeWrapper];}
-          ''
-            mkdir -p $out/bin
-            for i in ${jupyterlabEnvBase}/bin/*; do
-              filename=$(basename $i)
-
-              if [[ "$filename" == jupyter* ]]; then
-                cat <<EOF > $out/bin/$filename
-            #!${pkgs.runtimeShell} -e
-            trap "chmod +w --recursive \$PWD/.jupyter" EXIT
-            ${jupyterlab-cond-build}
-            ${jupyterlabEnvBase}/bin/$filename "\$@"
-            EOF
-                chmod +x $out/bin/$filename
-              else
-                makeWrapper \
-                  ${jupyterlabEnvBase}/bin/$filename \
-                  $out/bin/$filename
-            fi
-            done
-          '';
+        jupyterlabEnvWrapped = import (self + "/jupyterlab.nix");
 
         # Creates a derivation with kernel.json and logos
         mkKernel = kernelInstance_: let
