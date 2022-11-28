6f9f22e138237636e92aa213d609a0f73a91251d
Daniel Baker <daniel.baker@tweag.io>
2022-11-28 21:13:14 +0100

    add haskell compiler to runtime packages.

--- kernels/available/haskell/default.nix
+++ kernels/available/haskell/default.nix
@@ -4,19 +4,38 @@
   pkgs ? self.inputs.nixpkgs.legacyPackages.${system},
   name ? "haskell",
   displayName ? "Haskell",
+  runtimePackages ? [pkgs.haskell.compiler.${haskellCompiler}],
+  extraRuntimePackages ? [],
   haskellKernelPkg ? import "${self.inputs.ihaskell}/release.nix",
   haskellCompiler ? "ghc902",
   extraHaskellFlags ? "-M3g -N2",
   extraHaskellPackages ? (_: []),
 }: let
+  allRuntimePackages = runtimePackages ++ extraRuntimePackages;
+
   env = haskellKernelPkg {
     compiler = haskellCompiler;
     nixpkgs = pkgs;
     packages = extraHaskellPackages;
   };
-  kernelspec =
+
+  kernelspec = let
+    ihaskellGhcLib = env.ihaskellGhcLibFunc env.ihaskellExe env.ihaskellEnv;
+    wrappedEnv =
+      pkgs.runCommand "wrapper-${ihaskellGhcLib.name}"
+      {nativeBuildInputs = [pkgs.makeWrapper];}
+      ''
+        mkdir -p $out/bin
+        for i in ${ihaskellGhcLib}/bin/*; do
+          filename=$(basename $i)
+          ln -s ${ihaskellGhcLib}/bin/$filename $out/bin/$filename
+          wrapProgram $out/bin/$filename \
+            --set PATH "${pkgs.lib.makeSearchPath "bin" allRuntimePackages}"
+        done
+      '';
+  in
     env.ihaskellKernelFileFunc
-    (env.ihaskellGhcLibFunc env.ihaskellExe env.ihaskellEnv)
+    wrappedEnv
     extraHaskellFlags;
 in {
   inherit name displayName;
