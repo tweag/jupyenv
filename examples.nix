a638ea21795537b8e5c47ec7e8249348559feca3
Daniel Baker <daniel.baker@tweag.io>
2022-11-30 04:41:02 +0100

    cleanup examples file.

--- examples.nix
+++ examples.nix
@@ -1,91 +1,28 @@
-let
-  pkgs = import <nixpkgs> {};
-  lib = pkgs.lib;
-
-  mapKernelsFromPath = parentName: parentPath:
-  #builtins.listToAttrs
-  (
-    builtins.mapAttrs
-    (
-      {
-        name,
-        value,
-      }: {
-        inherit name;
-        value = value;
-        # path = parentPath + "/${name}";
-        # blah = lib.filesystem.listFilesRecursive (builtins.toPath path / name);
-      }
-    )
-    (getKernelAttrsetFromPath parentPath)
-  );
-
-  filterValidKernelPaths = parentPath: parentName: fileName: fileType: let
-    defaultFilePath = "${parentPath}/${parentName}/${fileName}/default.nix";
-  in
-    if
-      (fileType == "directory")
-      && !lib.pathExists defaultFilePath
-    then let
-      # subAttr = mapKernelsFromPath fileName (parentPath + "/${fileName}");
-      subAttr = getKernelAttrsetFromPath (parentPath + "/${parentName}") fileName;
-    in
-      # builtins.trace (subAttr // {inherit parentPath parentName fileName;})
-      {
-        # name = fileName;# + "-" + subAttr.name;
-        name = parentName + "-" + fileName + "-bar";
-        value = subAttr;
-      }
-    else if
-      (fileType == "directory")
-      && lib.pathExists defaultFilePath
-    then {
-      name = fileName;
-      # name = parentName + "-" + fileName + "-foo";
-      value = defaultFilePath;
-    }
-    else {
-      name = "null";
-      value = "null";
-    };
-
-  getKernelAttrsetFromPath = parentPath: parentName:
-    lib.mapAttrsToList
-    (filterValidKernelPaths parentPath parentName)
-    (builtins.readDir (parentPath + "/${parentName}"));
-
-  getKernels = parentPath: parentName:
+{lib}: let
+  getKernelListFromPath = parentPath: prevName:
     lib.mapAttrsToList
     (
       fileName: fileType: let
         parentString = builtins.toString parentPath;
         parentDir = lib.last (lib.splitString "/" parentString);
-        # subPath = getKernels (parentPath + "/${fileName}") parentDir;
 
         nextPath = parentPath + "/${fileName}";
         nextName =
-          if parentName == ""
+          if prevName == ""
           then parentDir
-          else (parentName + "-" + parentDir);
+          else (prevName + "-" + parentDir);
 
-        nextLevel = getKernels nextPath nextName;
+        nextLevel = getKernelListFromPath nextPath nextName;
         defaultNixFile = "${parentPath}/default.nix";
       in
         if
           (fileType == "directory")
           && !lib.hasPrefix "_" fileName
         then nextLevel
-        #{
-        #  # name = parentName;
-        #  name = nextName + "-" + fileName;
-        #  value = subPath;
-        #}
         else if
           (fileType == "regular")
           && lib.pathExists defaultNixFile
         then {
-          #name = fileName;
-          #value = fileType;
           name = nextName;
           value = defaultNixFile;
         }
@@ -93,22 +30,16 @@ let
     )
     (builtins.readDir parentPath);
 
-  getKernelAttrset = path: (
+  getKernelAttrsetFromPath = path: (
     builtins.listToAttrs (
       lib.remove
       null
       (
         lib.flatten
-        (getKernels path "")
+        (getKernelListFromPath path "")
       )
     )
   );
-in
-  # builtins.readDir ./kernels/example
-  # getKernelAttrsetFromPath ./kernels "example"
-  # mapKernelsFromPath "example" ./kernels/example
-  # mapKernelsFromPath "example" ./kernels
-  # (builtins.readDir ./kernels/example)
-  # getKernels ./kernels/example
-  #lib.remove null (lib.flatten (getKernels ./kernels/example ""))
-  getKernelAttrset ./kernels/example
+in {
+  inherit getKernelAttrsetFromPath;
+}
