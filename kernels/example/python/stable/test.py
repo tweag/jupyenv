292e314fce8277a6aeee935dffeeeb9b2812d74b
Daniel Baker <daniel.baker@tweag.io>
2022-11-30 18:58:37 +0100

    update python stable tests

--- kernels/example/python/stable/test.py
+++ kernels/example/python/stable/test.py
@@ -3,7 +3,7 @@ from testbook import testbook
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-@testbook(f'{current_dir}/test.ipynb', execute=True, kernel_name="example-stable-python-minimal")
+@testbook(f'{current_dir}/test.ipynb', execute=True, kernel_name="example-python-stable")
 def test_nb(tb):
     result = tb.cell_output_text(0)
     assert result == "5"
