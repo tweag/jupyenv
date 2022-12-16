48ae4b3a413788dd7b8b04b9ad07df08df485d8b
Daniel Baker <daniel.baker@tweag.io>
2022-12-16 08:05:36 +0100

    nest child options under parent in json object

--- docs/javascripts/options.js
+++ docs/javascripts/options.js
@@ -1,15 +1,34 @@
 function fetchOptions(path) {
   fetch(path)
     .then((response) => response.json())
+    .then((json) => nestJsonChildren(json))
     .then((json) => generateCommonMark(json))
     .then((json) => updateOptions(json))
 }
 
-function generateCommonMark(jsonObj) {
-  let markdown = "";
+function nestJsonChildren(jsonObj) {
+  let prevKey = "random prev key";
 
+  for (var key in jsonObj) {
+    if (key.includes(prevKey)) {
+      if (!jsonObj[prevKey].hasOwnProperty("children")) {
+        jsonObj[prevKey].children = {};
+      }
+      jsonObj[prevKey].children[key] = jsonObj[key];
+      delete jsonObj[key];
+    } else {
+      prevKey = key;
+    }
+  }
+
+  return jsonObj;
+}
+
+function generateCommonMark(jsonObj, markdown = "", header = "## ") {
   for (const [key, value] of Object.entries(jsonObj)) {
-    markdown = markdown.concat("## ", key.replace('<', '&lt;').replace('>', '&gt;'), "\n");
+
+    markdown = markdown.concat(header, key.replace('<', '&lt;').replace('>', '&gt;'), "\n");
+
     markdown = markdown.concat(value["description"], "\n\n");
     if ('type' in value) {
       markdown = markdown.concat("*_Type_*:", "\n");
@@ -26,6 +45,9 @@ function generateCommonMark(jsonObj) {
       markdown = markdown.concat("```", "\n");
       markdown = markdown.concat(value['example'], "\n\n");
       markdown = markdown.concat("```", "\n");
+    }
+    if ('children' in value) {
+      markdown = generateCommonMark(value["children"], markdown, "#" + header)
     }
   }
   markdown = markdown.replace(/(?:\\[rn])+/g, "\n");
