caf31e4ebc2ac3c804a9ddb45e9f0d1351c30ad3
Rok Garbas <rok@garbas.si>
2022-12-22 21:07:18 +0000

    adding a c kernel

--- modules/default.nix
+++ modules/default.nix
@@ -29,7 +29,7 @@ in {
 
   imports = [
     ./../kernels/available/bash/module.nix
-    #./../kernels/available/c/module.nix
+    ./../kernels/available/c/module.nix
     #./../kernels/available/elm/module.nix
     #./../kernels/available/go/module.nix
     #./../kernels/available/haskell/module.nix
