443d18513dec5de24589f5651160942b30485e86
GuangTao Zhang <gtrunsec@hardenedlinux.org>
2023-01-18 15:01:09 -0800

    Python: add ignoreCollisions argument  (#390)
    * python: add ignoreCollisions flag
    
    * Do not print the info on stdout
    
    Co-authored-by: Dan Baker <daniel.baker@tweag.io>

--- jupyterlab.nix
+++ jupyterlab.nix
@@ -51,11 +51,12 @@
       checker=$?
       if [ "$checker" -ne 0 ]
       then
-        echo jupyterWith needs to build JupyterLab.
+        >&2 echo "[$(date +'%Y-%m-%d %H:%M:%S') jupyterWith] needs to build JupyterLab."
         # we need to build the jupyter lab environment before it can be used
         ${jupyterlabEnvBase}/bin/jupyter lab build
       else
-        echo jupyterWith does not need build JupyterLab. Starting...
+        >&2 echo "[$(date +'%Y-%m-%d %H:%M:%S') jupyterWith] does not need build JupyterLab."
+        >&2 echo "[$(date +'%Y-%m-%d %H:%M:%S') jupyterWith] Starting..."
       fi
     '';
 in
