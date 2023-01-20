7b26a2e251af7cd68131f44011c8ddab6bd31560
Daniel Baker <daniel.baker@tweag.io>
2023-01-20 18:11:06 +0100

    fixed example python science kernel

--- kernels/example/python/science/default.nix
+++ kernels/example/python/science/default.nix
@@ -1,11 +1,6 @@
-{
-  availableKernels,
-  name,
-  extraArgs,
-}:
-availableKernels.python {
-  inherit name;
-  inherit (extraArgs) system;
-  displayName = "Example Python Kernel";
-  extraPackages = ps: [ps.numpy ps.scipy ps.matplotlib];
+{pkgs, ...}: {
+  kernel.python.minimal-example = {
+    enable = true;
+    extraPackages = ps: [ps.numpy ps.scipy ps.matplotlib];
+  };
 }
