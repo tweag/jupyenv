802d1673b28d508d1689cadef9313b1f69621460
Daniel Baker <daniel.baker@tweag.io>
2023-01-31 03:44:21 +0100

    making anchor links work better.

--- docs/python-scripts/options.py
+++ docs/python-scripts/options.py
@@ -381,11 +381,16 @@ def add_anchor(soup: BeautifulSoup, button: Tag, href: str) -> None:
         href: The content ID to link to in the anchor.
     '''
     anchor = soup.new_tag("a")
-    add_class(anchor, 'option-link')
+    add_class(anchor, 'option-link', 'md-button')
     anchor['href'] = '#' + href
-    anchor.append('#')
+    anchor['title'] = 'Permenant link'
+    anchor.append(get_svg_anchor(soup))
 
-    button.insert_after(anchor)
+    anchor_container = soup.new_tag("div")
+    add_class(anchor_container, 'option-link-container')
+
+    anchor_container.insert(0, anchor)
+    button.insert_after(anchor_container)
 
 
 def add_expand_all_button(soup: BeautifulSoup, elem: Tag) -> None:
@@ -432,6 +437,20 @@ def add_style(elem: Tag, style: str) -> None:
         style: The style to add.
     '''
     elem['style'] = elem.get('style', '') + style
+
+
+def get_svg_anchor(soup: BeautifulSoup) -> Tag:
+    elem = soup.new_tag("svg")
+    elem['aria-hidden'] = 'true'
+    elem['width'] = '1em'
+    elem['height'] = '1em'
+
+    svg_path = soup.new_tag("path")
+    svg_path['fill-rule'] = 'evenodd'
+    svg_path['d'] = 'M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z'
+
+    elem.insert(0, svg_path)
+    return elem
 
 
 if __name__ == '__main__':
