4d385d4402b3610f5f7b9f3a62f2bc1150c2e1f7
Pavel Anpin <pavel@anpin.fyi>
2024-12-02 12:30:41 -0300

    chore: bump flake inputs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   ];
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs.nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
   inputs.nixpkgs-master.url = "github:nixos/nixpkgs/master";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
@@ -17,7 +17,7 @@
   inputs.ihaskell.url = "github:ihaskell/ihaskell";
   inputs.ihaskell.inputs.flake-utils.follows = "flake-utils";
   inputs.ihaskell.inputs.nixpkgsMaster.follows = "nixpkgs-master";
-  inputs.ihaskell.inputs.nixpkgs24_05.follows = "nixpkgs-stable";
+  inputs.ihaskell.inputs.nixpkgs24_11.follows = "nixpkgs-stable";
   inputs.nix-dart.url = "github:djacu/nix-dart";
   inputs.nix-dart.inputs.nixpkgs.follows = "nixpkgs";
   inputs.nix-dart.inputs.flake-utils.follows = "flake-utils";
