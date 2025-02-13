76e5615a6391f91e951c94a1798c70fa529a5ad1
Pavel Anpin <pavel@anpin.fyi>
2025-02-13 16:49:02 -0300

    bump flake, poetry and fix deprecation warnings

--- modules/kernels/elm/overrides.nix
+++ modules/kernels/elm/overrides.nix
@@ -24,10 +24,10 @@ pkgs: let
       pypkgs-build-requirements)
     // {
       rpds-py = prev.rpds-py.overridePythonAttrs (old: {
-        cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
+        cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
           inherit (old) src;
           name = "${old.pname}-${old.version}";
-          hash = "sha256-VOmMNEdKHrPKJzs+D735Y52y47MubPwLlfkvB7Glh14=";
+          hash = "sha256-2skrDC80g0EKvTEeBI4t4LD7ZXb6jp2Gw+owKFrkZzc=";
         };
       });
     });
